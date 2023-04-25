3019d065d1621bc857faaa8a76e990d46c295fa9
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-25 12:47:35 -0300

    feat: Relationships and Routes (has_many)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
   end
+  
+  resources :contacts do 
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/phones'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
