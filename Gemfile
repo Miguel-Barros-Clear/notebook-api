39d19d9c48e1b3392f056a8631cada5da21ceecc
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-25 11:26:45 -0300

    feat: adding foreman and creating procfile archive

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'sqlite3'
 gem 'rails-i18n'
 gem 'pry-rails'
 gem 'active_model_serializers'
+gem 'foreman'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,12 +1,11 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate #, :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { contact_kind_url(object.id) }
+  end
   has_many :phones
   has_one :address
-
-  link(:self) { contact_url(object.id) }
-  link(:kind) { kind_url(object.kind.id) }
 
   meta do
     {
