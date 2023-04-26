61d801675987e1f434092320cb6cfbc57f2bf6a6
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-26 12:16:57 -0300

    feat: gem versionist

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'pry-rails'
 gem 'active_model_serializers'
 gem 'foreman'
 gem 'jwt'
+gem 'versionist'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
---
+++ app/controllers/v2/phones_controller.rb
@@ -0,0 +1,42 @@
+module V2
+    class PhonesController < ApplicationController
+        before_action :set_address
+
+        def destroy
+            phone = Phone.find(phone_params[:id])
+            phone.destroy
+        end
+
+        def create
+            @contact.phones << Phone.new(phone_params)
+
+            if @contact.save
+                render json: @contact.phones, status: :created, location: contact_phone_url(@contact)
+            else
+                render json: @contact.errors, status: :unprocessable_entity
+            end
+        end
+
+        def update
+            if @contact.phones.update(phone_params)
+                render json: @contact.phones
+            else
+                render json: @contact.errors, status: :unprocessable_entity
+            end
+        end
+
+        def show
+            render json: @contact.phones
+        end
+
+        private
+            # Use callbacks to share common setup or constraints between actions.
+            def set_address
+                @contact = Contact.find(params[:contact_id])
+            end
+
+            def phone_params
+                ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+            end
+    end
+end
